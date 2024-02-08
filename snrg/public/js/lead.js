94b96d38c4a5e9fc878f0a7fa6e8e4b5f374ea1a
sudarsan2001 <frankel9675@gmail.com>
2024-02-08 18:25:40 +0530

    Update GSTIN field for Lead DocType

--- snrg/public/js/lead.js
+++ snrg/public/js/lead.js
@@ -13,19 +13,20 @@ frappe.ui.form.on("Lead", {
         set_custom_gstin_description(custom_gstin_field, gstin_status.status);
         console.log("set_custom_description===========".set_custom_gstin_description)
 
-        function get_gstin_status(gstin) {
-            return frappe
-                .call({
-                    method: "india_compliance.gst_india.doctype.gstin.gstin.get_gstin_status",
-                    args: {gstin}
-                })
-                .then(r => r.message);
-        }
+        
 
-        function set_custom_gstin_description(gstin_field, status) {
-            console.log("status====================================================================",status)
-            gstin_field.set_description(india_compliance.get_gstin_status_desc(status));
-        }
-
+        
     }
 })
+function get_gstin_status(gstin) {
+    return frappe
+        .call({
+            method: "india_compliance.gst_india.doctype.gstin.gstin.get_gstin_status",
+            args: {gstin}
+        })
+        .then(r => r.message);
+}
+function set_custom_gstin_description(gstin_field, status) {
+    console.log("status====================================================================",status)
+    gstin_field.set_description(india_compliance.get_gstin_status_desc(status));
+}
